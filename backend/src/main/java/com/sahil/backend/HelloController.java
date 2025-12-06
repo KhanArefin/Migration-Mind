f5a51fb03dfac5e8422658d845fab79d41b289ec
Khan Arefin <k.shamsarefin123@gmail.com>
2025-12-06 22:27:01 +0600

    created 2 new API's /user, /course

--- backend/src/main/java/com/sahil/backend/HelloController.java
+++ backend/src/main/java/com/sahil/backend/HelloController.java
@@ -1,14 +1,39 @@
 package com.sahil.backend;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
-public class HelloController {
+public class HelloController 
+{
+    
     @GetMapping("/hello")
-    public String sayHello() {
+    public String sayHello() 
+    {
         return "Hello, World!";
     }
     
+    @GetMapping("/user")
+    public Map<String, String> getUser() 
+    {
+        Map<String, String> user = new HashMap<>();
+        user.put("name", "Sahil");
+        user.put("age", "22");
+        return user;
+    }
+
+    @GetMapping("/course")
+    public Map<String, String> getCourse() 
+    {
+        Map<String, String> user = new HashMap<>();
+        user.put("name", "AOOP");
+        user.put("section", "A");
+        return user;
+    }
 }
+
